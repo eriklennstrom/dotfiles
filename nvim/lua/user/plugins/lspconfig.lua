20bfe2da1803d6dddaff5058d92bdc0ae75b00d6
Erik Tedfelt Lennström <erik@tedfeltlennstrom.se>
2023-02-20 22:21:07 +0100

    wip: linting, add: lsp

--- nvim/lua/user/plugins/lspconfig.lua
+++ nvim/lua/user/plugins/lspconfig.lua
@@ -1,11 +1,46 @@
 -- Setup Mason to automatically install LSP servers
 require('mason').setup()
 require('mason-lspconfig').setup({ automatic_installation = true })
-
+-- nvim-cmp supports additional completion capabilities
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- JS/TS/VUE/REACT
- require'lspconfig'.volar.setup{
+require('lspconfig').volar.setup{
+  capabilities = capabilities,
   filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json'}
 }
+require('lspconfig').jsonls.setup({
+  capabilities = capabilities,
+  settings = {
+    json = {
+      schemas = require('schemastore').json.schemas(),
+    },
+  },
+})
+
+require('null-ls').setup({
+  sources = {
+    require('null-ls').builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.code_actions.gitsigns,
+    require('null-ls').builtins.diagnostics.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+    require('null-ls').builtins.diagnostics.gitlint,
+    require('null-ls').builtins.diagnostics.trail_space.with({ disabled_filetypes = { 'NvimTree' } }),
+    require('null-ls').builtins.formatting.eslint_d.with({
+      condition = function(utils)
+        return utils.root_has_file({ '.eslintrc.js' })
+      end,
+    }),
+  },
+})
+
+require('mason-null-ls').setup({ automatic_installation = true })
 
 -- Keymaps
 vim.keymap.set('n', '<Leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>')
@@ -17,6 +52,10 @@ vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>')
 vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
 vim.keymap.set('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>')
 
+-- Commands
+vim.api.nvim_create_user_command('Format', vim.lsp.buf.formatting, {})
+
+-- Diagnostic config
 vim.diagnostic.config({
   virtual_text = false,
   severity_sort = true,
@@ -25,7 +64,7 @@ vim.diagnostic.config({
     focus = false,
     format = function(diagnostic)
       if diagnostic.user_data ~= nil and diagnostic.user_data.lsp.code ~= nil then
-        return string.format('%s: %s', diagnostic.user_data.lsp.code, diagnostic.message)
+        return string.format('%s: %s', diagnostic.user_data.lsp.code, diagnostic.m)
       end
       return diagnostic.message
     end,
