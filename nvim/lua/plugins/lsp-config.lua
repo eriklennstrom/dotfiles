3ce16d39145c345b0029a69549a808323cdead94
Erik Tedfelt Lennström <erik@tedfeltlennstrom.se>
2024-03-18 21:37:56 +0100

    add completions

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -22,9 +22,14 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.phpactor.setup({})
+      lspconfig.lua_ls.setup({
+        capabilites = capabilities
+      })
+      lspconfig.phpactor.setup({
+        capabilites = capabilities
+      })
       lspconfig.eslint.setup({
         settings = {
           codeActionOnSave = {
@@ -32,11 +37,15 @@ return {
             mode = "all",
           },
         },
+        capabilites = capabilities
       })
       lspconfig.volar.setup({
         filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+        capabilites = capabilities
       })
-      lspconfig.tsserver.setup({})
+      lspconfig.tsserver.setup({
+        capabilites = capabilities
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
