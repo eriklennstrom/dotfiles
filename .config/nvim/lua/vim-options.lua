38dda4bd196e24d0f0d95bb84f2a1a497a831453
Erik Tedfelt Lennström <erik@tedfeltlennstrom.se>
2024-03-27 00:13:58 +0100

    add tmux theme, fix navigation tmux vim

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -11,3 +11,9 @@ vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamed,unnamedplus"
 vim.o.winbar = "%{%v:lua.require'utils.winbar'.eval()%}"
 -- vim.cmd.colorscheme "tokyo-night"
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
